cd3857c01c2247b4eef4efb143a3f1d8ff210b74
bvsspavankumar <bvsspavankumar@gmail.com>
2020-12-20 16:40:23 +0530

    Adding handler for deleting a post

--- api/delete_note.js
+++ api/delete_note.js
@@ -12,11 +12,19 @@ const tableName = process.env.NOTES_TABLE;
 
 exports.handler = async event => {
     try {
+        let timestamp = parseInt(event.pathParameters.timestamp)
+        let params = {
+            TableName: tableName,
+            Key: {
+                user_id: util.getUserId(event.headers),
+                timestamp: timestamp
+            }
+        }
+        await dynamodb.delete(params).promise()
 
         return {
             statusCode: 200,
             headers: util.getResponseHeaders(),
-            body: Json.stringify('')
         }
     } catch (err) {
         console.log("Error", err)
